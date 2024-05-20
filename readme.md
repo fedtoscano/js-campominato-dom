55ddeae70a397d27aa2cf535ceb1fe05478d44c9
fedtoscano <federicotoscano611@gmail.com>
2024-05-20 12:22:28 +0200

    add bomb list feature and randomInt function

--- readme.md
+++ readme.md
@@ -15,3 +15,15 @@ con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 
 con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
+
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+        creo un numero randomico fra 1 e il numero massimo di bombe a cui ho accesso in quella difficoltà
+        inizializzo un array (vuoto) che contiene i numeri già estratti come bomba
+        
+        FINO A CHE IL NUMERO DI ELEMENTI NELL'ARRAY è MINORE DI 16:
+            se l'array contiene il numero generato, continua a generarne altri
+            quando trovi un numero che non è già presente nell'array, pushalo all'interno di esso
+
--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 
 const startBtn = document.querySelector("button#start-game")
+// ? const resetBtn?
 
 function gameStart(){
 
     const mainEl = document.querySelector("main");
     const gridEl = document.createElement("div");
     gridEl.id = "grid";
-
+    mainEl.innerHTML = ""
     mainEl.appendChild(gridEl);
 
     const selectDifficultyEl = document.querySelector("#select-difficulty");
@@ -32,6 +33,7 @@ function gameStart(){
                 newSquare.classList.add("square-hard")
             }
 
+                //! questa va riscritta con la funzione "vera" per le bombe
         newSquare.addEventListener('click', function(){
             this.classList.add("bg-light-blue")
             console.log(this.innerText)
@@ -43,7 +45,28 @@ function gameStart(){
         newSquare.appendChild(textContent)
         gridEl.appendChild(newSquare)
     }
+    
+    let bombList = [];
+    for (let index = 0; index < 16; index++) {
+
+        let randomNum = makeRandomInt(1, numberOfSquares)
+        while (bombList.includes(randomNum)!==true) {
+            randomNum = makeRandomInt(1, numberOfSquares)
+            bombList.push(randomNum);
+        }
+        
+    }
+    console.log(bombList)
     // button.disabled = true
+}
+/**returns a random number between a min and a max value
+ * 
+ * @param {*} min the min number   
+ * @param {*} max the max number
+ * @returns random integer between min and max
+ */
+function makeRandomInt(min, max){
+    return Math.floor(Math.random() * ((max - min) +1) + min)
 }
 
 startBtn.addEventListener('click', gameStart)
